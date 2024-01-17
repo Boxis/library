e86b76dcbaa54bb66ef052934dd862e443b62d8d
Boxi Z <8356799+Boxis@users.noreply.github.com>
2024-01-17 14:22:12 -0500

    Add form and submit button script

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-// const myLibrary = [];
+const myLibrary = [];
 
-// function Book() {
-//     // the constructor
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-//     this.info()  = function () {
-//         return this.title + ' by ' + this.author + ', ' + this.pages 
-//                + ', ' + this.read;
-//     }
-// }
+function getBookInfo () {
 
-// function addBookToLibrary () {
-//     // do stuff here
-// }
+  var author = document.getElementById("author").value;
+  var title = document.getElementById("title").value;
+  var pages = document.getElementById("pages").value;
+  var read = document.querySelector('input[name="read"]:checked').value;
+
+  console.log(title, author, pages, read);
+
+  const book1 = new Book(title, author, pages, read);
+
+  dialog.close();
+}
+
+function Book() {
+    // the constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info  = function () {
+        return this.title + ' by ' + this.author + ', ' + this.pages 
+               + ', ' + this.read;
+    };
+}
+
+function addBookToLibrary () {
+    // do stuff here
+}
 
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
+const closeButton = document.querySelector("#close-btn");
+const submitButton = document.querySelector("#submit-btn");
 
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
   dialog.showModal();
 });
+
+// Submit button stores values entered in form
+submitButton.addEventListener('click', getBookInfo, false);
 
 // "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
