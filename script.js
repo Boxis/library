1c18d075f2bd6f0c43719706e7fafee15aa27aa8
Boxi Z <8356799+Boxis@users.noreply.github.com>
2024-01-26 12:38:17 -0500

    Add logic for delete button

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
 let myLibrary = [];
 
 // Example books for array
-const book1 = new Book ("Book A", "Author A", 123, "yes");
-const book2 = new Book ("Book B", "Author B", 123, "no");
-const book3 = new Book ("Book C", "Author C", 11223, "yes");
-const book4 = new Book ("Book D", "Author D", 11223, "yes");
+const book1 = new Book ("Book A", "Author A", 123, "yes", 0);
+const book2 = new Book ("Book B", "Author B", 123, "no", 1);
+const book3 = new Book ("Book C", "Author C", 11223, "yes", 2);
+const book4 = new Book ("Book D", "Author D", 11223, "yes", 3);
 
 myLibrary.push(book1, book2, book3, book4);
 
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, bookID) {
   // the constructor
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.bookID = bookID;
 }
 
+let len = myLibrary.length;
+let j = len;
 
 function getBookInfo () { 
   const title_val = document.getElementById("title").value;
   const author_val = document.getElementById("author").value;
   const pages_val = document.getElementById("pages").value;
   const read_val = document.querySelector('input[name="read"]:checked').value;
-  const book = new Book(title_val, author_val, pages_val, read_val);
+  const bookID_val = j;
+  const book = new Book(title_val, author_val, pages_val, read_val, bookID_val);
 
   myLibrary.push(book);
 
   dialog.close();
 
+  j++;
   len = myLibrary.length;
   addBookToLibrary();
 }
 
 
-
 function addBookToLibrary () {
   // do stuff here
-  let bookCards = document.querySelectorAll('.book-card').length;
-  console.log(bookCards);
-  console.log(i);
-  i = bookCards;
-  console.log(i);
-
-  for (; i < len; i++) {
+  document.querySelector('.library-area').innerHTML='';
+  for (let i = 0; i < len; i++) {
     createLibCard(i);
   }
+  // console.log(myLibrary);
 }
 
 
 function createLibCard (i) {
   const libraryArea = document.querySelector('.library-area');
-  console.log(libraryArea);
+  // console.log(libraryArea);
   
   // create a new div element
   const newDiv = document.createElement("div");
   newDiv.classList.add('book-card');
-  // newDiv.id = "book-" + i;
+  newDiv.id = "book-" + i;
 
   const title = document.createElement('p');
   title.innerText = "Title: " + myLibrary[i].title;
@@ -71,6 +71,8 @@ function createLibCard (i) {
   read.id = "read";
   read.innerText = "Read: " + myLibrary[i].read;
 
+  const bookID = myLibrary[i].bookID;
+
   var btnRead = document.createElement("button");
   btnRead.innerText = "Read?";
   btnRead.onclick = function(){
@@ -83,13 +85,11 @@ function createLibCard (i) {
 
   var btnDel = document.createElement("button");
   btnDel.innerText = "Delete";
+  btnDel.class = "delBtn";
   btnDel.onclick = function(){
-    newDiv.parentNode.removeChild(newDiv)
-    delBooks = myLibrary.splice(i, 1);
-    len = myLibrary.length;
-    ; return false;
-  };
-
+    myLibrary = myLibrary.filter(book => book.bookID !==  bookID);
+    newDiv.parentNode.removeChild(newDiv);
+  }
 
   // add the text node to the newly created div
   newDiv.appendChild(title);
@@ -102,8 +102,6 @@ function createLibCard (i) {
   libraryArea.appendChild(newDiv);
 }
 
-let i = 0;
-let len = myLibrary.length;
 addBookToLibrary();
 
 
@@ -119,12 +117,6 @@ showButton.addEventListener("click", () => {
 
 // Submit button stores values entered in form
 submitButton.addEventListener('click', getBookInfo);
-// submitButton.addEventListener("keypress" , function(e){ 
-//   var key = e.which;
-//   if(key == 13) {
-//     getBookInfo();
-//   }
-// });
 
 // "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
